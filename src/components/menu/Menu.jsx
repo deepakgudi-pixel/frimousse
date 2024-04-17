eed02e00997744ebededaaf9013da117a714d082
deepakgudi-pixel <deepak.g@supercode.in>
2024-04-17 12:14:14 +0530

    menu animation

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -15,86 +15,166 @@ import LinkPreviewImg4 from "../../assets/images/menu/link-4.jpg";
 export default function Menu() {
 
 
-    const menuLinks = [
+  const menuLinks = [
 
-        { path: "/about", label: "About" },
-        { path: "/works", label: "Work" },
-        { path: "/", label: "Home" },
-        { path: "/contact", label: "Contact" },
-        
+    { path: "/about", label: "About" },
+    { path: "/works", label: "Work" },
+    { path: "/", label: "Home" },
+    { path: "/contact", label: "Contact" },
+
+  ];
+
+
+  const menuContainer = useRef();
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const menuAnimation = useRef();
+  const menuLinksAnimation = useRef();
+  const revealHoveredLinkImg = useRef();
+
+  const toggleMenu = () => {
+    document.querySelector('.hamburger-icon').classList.toggle("active");
+    setIsMenuOpen(!isMenuOpen)
+  };
+
+
+  useEffect(() => {
+    gsap.set('.menu-link-item-holder', { y: 125 });
+
+    menuAnimation.current = gsap.timeline({ paused: true }).to(".menu", {
+      duration: 1,
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      ease: "power4.inOut",
+    });
+
+    menuLinksAnimation.current = gsap
+      .timeline({ paused: true })
+      .to(".menu-link-item-holder", {
+        y: 0,
+        duration: 1.25,
+        stagger: 0.075,
+        ease: "power3.inOut",
+        delay: 0.125,
+      });
+
+    revealHoveredLinkImg.current = gsap
+      .timeline({ paused: true })
+      .to(".bind-new-img", {
+        top: "0%",
+        duration: 1,
+        ease: "power.out",
+      });
+
+  }, []);
+
+  useEffect(() => {
+
+    if (isMenuOpen) {
+      menuAnimation.current.play();
+      menuLinksAnimation.current.play();
+    } else {
+      menuAnimation.current.reverse();
+      menuLinksAnimation.current.reverse();
+    }
+
+  }, [isMenuOpen])
+
+  //link hover animation
+  useEffect(() => {
+    const previewContainer = document.querySelector(".link-preview-img");
+    const menuLinkItems = document.querySelectorAll(".menu-link-item");
+    const linkImages = [
+      LinkPreviewImg1,
+      LinkPreviewImg2,
+      LinkPreviewImg3,
+      LinkPreviewImg4,
     ];
 
+    let lastHoveredIndex = null;
 
-    const menuContainer = useRef();
-    const [isMenuOpen, setIsMenuOpen] = useState(false)
-    const menuAnimation = useRef();
-    const menuLinksAnimation = useRef();
-    const revealHoveredLinkImg = useRef();
+    const handleMouseOver = (index) => {
 
-    const toggleMenu = () => {
-      document.querySelector('.hamburger-icon').classList.toggle("active");
-      setIsMenuOpen(!isMenuOpen)
+      if (index !== lastHoveredIndex) {
+
+        const imgContainer = document.createElement("div");
+        imgContainer.classList.add("bind-new-img");
+        const img = document.createElement("img");
+        img.src = linkImages[index];
+        img.alt = "";
+        imgContainer.appendChild(img);
+        previewContainer.appendChild(imgContainer);
+
+
+        gsap.to(imgContainer, {
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          duration: 1.25,
+          ease: "power3.out",
+
+          onComplete: () => {
+
+            gsap.delayedCall(2, () => {
+              const allImgContainers = previewContainer.querySelectorAll(".bind-new-img");
+
+              if (allImgContainers.length > 1) {
+                Array.from(allImgContainers)
+                  .slice(0, -1)
+                  .forEach((container) => {
+                    setTimeout(() => {
+                      container.remove();
+                    }, 2000);
+                  });
+              }
+
+            })
+
+          },
+
+        })
+
+
+        lastHoveredIndex = index;
+
+      }
+
     };
 
-    
-    useEffect(()=> {
-        gsap.set('.menu-link-item-holder', { y: 125});
+    menuLinkItems.forEach((item, index) => {
+      item.addEventListener("mouseover", () => handleMouseOver(index));
+    })
 
-        menuAnimation.current = gsap.timeline({ paused: true }).to(".menu", {
-            duration: 1,
-            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-            ease: "power4.inOut",
-        });
+    return () => {
+      menuLinkItems.forEach((item) => {
+        item.removeEventListener("mouseover", () => handleMouseOver(index));
+      });
+    };
 
-        menuLinksAnimation.current = gsap
-          .timeline({ paused: true })
-          .to(".menu-link-item-holder", {
-             y: 0,
-             duration: 1.25,
-             stagger: 0.075,
-             ease: "power3.inOut",
-             delay: 0.125,
-          });
+  }, [])
 
-    }, []);
 
-    useEffect(()=>{
 
-        if(isMenuOpen){
-            menuAnimation.current.play();
-            menuLinksAnimation.current.play();
-        } else {
-            menuAnimation.current.reverse();
-            menuLinksAnimation.current.reverse(); 
-        }
-
-    }, [isMenuOpen])
 
 
   return (
     <div className="menu-container" ref={menuContainer}>
-        
-        <div className="menu-bar">
 
-         <div className="menu-actions">
+      <div className="menu-bar">
 
-           <div className="menu-toggle">
-           <button className="hamburger-icon" onClick={toggleMenu}></button>
-           </div>
-         </div>
+        <div className="menu-actions">
+
+          <div className="menu-toggle">
+            <button className="hamburger-icon" onClick={toggleMenu}></button>
+          </div>
+        </div>
+      </div>
+
+      <div className="menu">
+        <div className="link-preview-img">
+          <img src={DefaultPreviewImg} alt="" />
+          <div className="bind-new-img">
+            <img src={LinkPreviewImg1} alt="" />
+          </div>
         </div>
 
-        <div className="menu">
-            <div className="link-preview-img">
-
-            </div>
-
-           <div className="bind-new-img">
-              
-           </div>
-
-
-           <div className="menu-col">
+        <div className="menu-col">
           <div className="menu-sub-col">
             <div className="menu-links">
               {menuLinks.map((link, index) => (
@@ -113,9 +193,9 @@ export default function Menu() {
             </div>
           </div>
         </div>
-         
-        
-        </div>
+
+
+      </div>
     </div>
   )
 }
